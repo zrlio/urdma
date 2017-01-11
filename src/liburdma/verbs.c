zrlio/urdma

commit 18667e1c0ad945228f2bee1d8a05be629e6e16a3

liburdma, kmod: Update for urdmad
Update liburdma to act as a secondary process to rdmad.

Signed-off-by: Patrick MacArthur <patrick@patrickmacarthur.net>

src/liburdma/verbs.c
@@ -55,9 +55,7 @@
 #include <rte_malloc.h>
 #include <rte_ring.h>
 
-#include "config_file.h"
 #include "interface.h"
-#include "kni.h"
 #include "urdma_kabi.h"
 #include "util.h"
 #include "verbs.h"
@@ -179,7 +177,8 @@ urdma_accl_post_send(struct ibv_qp *ib_qp, void *addr, size_t length,
 static bool
 qp_connected(struct usiw_qp *qp)
 {
-	return rte_atomic16_read(&qp->conn_state) == usiw_qp_connected;
+	uint16_t tmp = rte_atomic16_read(&qp->shm_qp->conn_state);
+	return tmp == usiw_qp_connected || tmp == usiw_qp_running;
 } /* qp_connected */
 
 
@@ -203,7 +202,7 @@ urdma_accl_post_sendv(struct ibv_qp *ib_qp, struct iovec *iov, size_t iov_size,
 		return -EINVAL;
 	}
 
-	ee = usiw_get_ee_context(qp, ah);
+	ee = &qp->remote_ep;
 	if (!ee) {
 		return -EINVAL;
 	}
@@ -248,7 +247,7 @@ urdma_accl_post_write(struct ibv_qp *ib_qp, void *addr, size_t length,
 		return -EINVAL;
 	}
 
-	ee = usiw_get_ee_context(qp, ah);
+	ee = &qp->remote_ep;
 	if (!ee) {
 		return -EINVAL;
 	}
@@ -293,7 +292,7 @@ urdma_accl_post_read(struct ibv_qp *ib_qp, void *addr, size_t length,
 		return -EINVAL;
 	}
 
-	ee = usiw_get_ee_context(qp, ah);
+	ee = &qp->remote_ep;
 	if (!ee) {
 		return -EINVAL;
 	}
@@ -343,7 +342,7 @@ usiw_query_device(struct ibv_context *context,
 	device_attr->vendor_id = URDMA_VENDOR_ID;
 	device_attr->vendor_part_id = URDMA_VENDOR_PART_ID;
 	device_attr->hw_ver = 0;
-	device_attr->max_qp = DPDKV_MAX_QP;
+	device_attr->max_qp = DPDKV_MAX_QP - 1;
 	device_attr->max_qp_wr = RTE_MIN(MAX_SEND_WR, MAX_RECV_WR);
 	device_attr->device_cap_flags = 0;
 	device_attr->max_sge = DPDK_VERBS_IOV_LEN_MAX;
@@ -736,19 +735,40 @@ usiw_post_srq_recv(__attribute__((unused)) struct ibv_srq *srq,
 } /* usiw_post_srq_recv */
 
 
-static struct usiw_qp *
-port_get_next_qp(struct usiw_port *port)
+static struct urdmad_qp *
+port_get_next_qp(struct usiw_device *dev)
 {
-	void *result;
-	int ret;
+	struct urdmad_sock_qp_msg msg;
+	ssize_t ret;
 
-	ret = rte_ring_dequeue(port->avail_qp, &result);
-	if (ret) {
-		errno = -ret;
+	memset(&msg, 0, sizeof(msg));
+	msg.hdr.opcode = rte_cpu_to_be_32(urdma_sock_create_qp_req);
+	msg.hdr.dev_id = rte_cpu_to_be_16(dev->portid);
+	ret = send(dev->urdmad_fd, &msg, sizeof(msg), 0);
+	if (ret < sizeof(msg)) {
 		return NULL;
 	}
-	return result;
+	ret = recv(dev->urdmad_fd, &msg, sizeof(msg), 0);
+	if (ret < sizeof(msg) || rte_be_to_cpu_32(msg.hdr.opcode)
+						!= urdma_sock_create_qp_resp) {
+		return NULL;
+	}
+	return (struct urdmad_qp *)(uintptr_t)rte_be_to_cpu_64(msg.ptr);
 } /* port_get_next_qp */
+
+
+static void
+port_return_qp(struct usiw_qp *qp)
+{
+	struct urdmad_sock_qp_msg msg;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.hdr.opcode = rte_cpu_to_be_32(urdma_sock_destroy_qp_req);
+	msg.hdr.dev_id = rte_cpu_to_be_16(qp->dev->portid);
+	msg.hdr.qp_id = rte_cpu_to_be_16(qp->shm_qp->qp_id);
+	msg.ptr = rte_cpu_to_be_64((uintptr_t)qp->shm_qp);
+	send(qp->dev->urdmad_fd, &msg, sizeof(msg), 0);
+} /* port_return_qp */
 
 
 static struct ibv_qp *
@@ -763,6 +783,7 @@ usiw_create_qp(struct ibv_pd *pd, struct ibv_qp_init_attr *qp_init_attr)
 		struct urdma_uresp_create_qp priv;
 	} resp;
 	struct usiw_context *ctx;
+	struct ee_state *ee;
 	struct usiw_qp *qp;
 	int retval;
 
@@ -801,29 +822,34 @@ usiw_create_qp(struct ibv_pd *pd, struct ibv_qp_init_attr *qp_init_attr)
 
 	ctx = usiw_get_context(pd->context);
 
-	qp = port_get_next_qp(ctx->port);
+	qp = malloc(sizeof(*qp));
 	if (!qp) {
 		goto errout;
 	}
+	qp->shm_qp = port_get_next_qp(ctx->dev);
+	if (!qp->shm_qp) {
+		goto free_user_qp;
+	}
 
 	/* Create kernel QP for connection manager */
-	cmd.priv.ird_max = qp->ird_max = USIW_IRD_MAX;
-	cmd.priv.ord_max = qp->ord_max = USIW_ORD_MAX;
+	cmd.priv.urdmad_dev_id = ctx->dev->portid;
+	cmd.priv.urdmad_qp_id = qp->shm_qp->qp_id;
+	cmd.priv.ird_max = qp->shm_qp->ird_max = USIW_IRD_MAX;
+	cmd.priv.ord_max = qp->shm_qp->ord_max = USIW_ORD_MAX;
+	cmd.priv.rxq = qp->shm_qp->rx_queue;
+	cmd.priv.txq = qp->shm_qp->tx_queue;
 	retval = ibv_cmd_create_qp(pd, &qp->ib_qp, qp_init_attr,
 			&cmd.ibv, sizeof(cmd), &resp.ibv, sizeof(resp));
 	if (retval != 0) {
 		errno = retval;
-		goto free_user_qp;
+		goto return_user_qp;
 	}
 
 	qp->qp_flags = qp_init_attr->sq_sig_all
 		? usiw_qp_sig_all : 0;
-	rte_atomic16_init(&qp->conn_state);
-	rte_atomic16_set(&qp->conn_state, usiw_qp_unbound);
-	qp->qp_state = IBV_QPS_INIT;
-	rte_spinlock_init(&qp->conn_event_lock);
+	rte_atomic16_set(&qp->shm_qp->conn_state, usiw_qp_unbound);
 	qp->ctx = ctx;
-	qp->port = ctx->port;
+	qp->dev = ctx->dev;
 	qp->stats.recv_max_burst_size = RX_BURST_SIZE;
 	qp->stats.recv_count_histo = calloc(qp->stats.recv_max_burst_size + 1,
 			sizeof(*qp->stats.recv_count_histo));
@@ -842,7 +868,6 @@ usiw_create_qp(struct ibv_pd *pd, struct ibv_qp_init_attr *qp_init_attr)
 	qp->txq_end = qp->txq;
 	qp->timer_last = 0;
 	qp->pd = container_of(pd, struct usiw_mr_table, pd);
-
 
 	retval = usiw_send_wqe_queue_init(qp->ib_qp.qp_num,
 			&qp->sq, qp_init_attr->cap.max_send_wr,
@@ -866,9 +891,21 @@ usiw_create_qp(struct ibv_pd *pd, struct ibv_qp_init_attr *qp_init_attr)
 	TAILQ_INIT(&qp->readresp_empty);
 	qp->ird_active = 0;
 
-	if (sem_init(&qp->conn_event_sem, 0, 0) < 0) {
+	ee = &qp->remote_ep;
+	/* FIXME: Get this from the peer */
+	ee->send_max_psn = ee->tx_pending_size = qp->dev->rx_desc_count / 2;
+	ee->tx_pending = calloc(ee->send_max_psn, sizeof(*ee->tx_pending));
+	if (!ee->tx_pending) {
 		goto free_kernel_qp;
 	}
+	ee->expected_recv_msn = 1;
+	ee->expected_read_msn = 1;
+	ee->expected_ack_msn = 1;
+	ee->next_send_msn = 1;
+	ee->next_read_msn = 1;
+	ee->next_ack_msn = 1;
+
+	ee->tx_head = ee->tx_pending;
 
 	rte_atomic32_init(&qp->refcnt);
 	rte_atomic32_set(&qp->refcnt, 1);
@@ -879,12 +916,15 @@ usiw_create_qp(struct ibv_pd *pd, struct ibv_qp_init_attr *qp_init_attr)
 			sizeof(qp->ib_qp.qp_num), qp);
 	rte_spinlock_unlock(&ctx->qp_lock);
 
+	LIST_INSERT_HEAD(&qp->ctx->qp_active, qp, ctx_entry);
 	return &qp->ib_qp;
 
 free_kernel_qp:
 	ibv_cmd_destroy_qp(&qp->ib_qp);
+return_user_qp:
+	port_return_qp(qp);
 free_user_qp:
-	rte_ring_enqueue(ctx->port->avail_qp, qp);
+	free(qp);
 errout:
 	return NULL;
 } /* usiw_create_qp */
@@ -906,11 +946,12 @@ usiw_query_qp(struct ibv_qp *ib_qp, struct ibv_qp_attr *attr, int attr_mask,
 	}
 
 	qp = container_of(ib_qp, struct usiw_qp, ib_qp);
-	switch (rte_atomic16_read(&qp->conn_state)) {
+	switch (rte_atomic16_read(&qp->shm_qp->conn_state)) {
 	case usiw_qp_unbound:
 		attr->qp_state = IBV_QPS_INIT;
 		break;
 	case usiw_qp_connected:
+	case usiw_qp_running:
 		attr->qp_state = IBV_QPS_RTS;
 		break;
 	case usiw_qp_shutdown:
@@ -974,7 +1015,7 @@ usiw_modify_qp(struct ibv_qp *ib_qp, struct ibv_qp_attr *attr, int attr_mask)
 	switch (attr->qp_state) {
 	case IBV_QPS_SQD:
 	case IBV_QPS_ERR:
-		rte_atomic16_set(&qp->conn_state, usiw_qp_shutdown);
+		rte_atomic16_set(&qp->shm_qp->conn_state, usiw_qp_shutdown);
 		break;
 	default:
 		break;
@@ -994,7 +1035,7 @@ usiw_destroy_qp(struct ibv_qp *ib_qp)
 
 	qp = container_of(ib_qp, struct usiw_qp, ib_qp);
 	ctx = qp->ctx;
-	if (rte_atomic16_read(&qp->conn_state) == usiw_qp_unbound) {
+	if (rte_atomic16_read(&qp->shm_qp->conn_state) == usiw_qp_unbound) {
 		assert(rte_atomic32_read(&ctx->qp_init_count) != 0);
 		rte_atomic32_dec(&ctx->qp_init_count);
 	}
@@ -1002,7 +1043,7 @@ usiw_destroy_qp(struct ibv_qp *ib_qp)
 	if (qp->send_cq != qp->recv_cq) {
 		qp->send_cq->qp_count--;
 	}
-	rte_atomic16_set(&qp->conn_state, usiw_qp_error);
+	rte_atomic16_set(&qp->shm_qp->conn_state, usiw_qp_error);
 
 	rte_spinlock_lock(&ctx->qp_lock);
 	HASH_DEL(ctx->qp, qp);
@@ -1054,26 +1095,14 @@ usiw_post_send(struct ibv_qp *ib_qp, struct ibv_send_wr *wr,
 	}
 
 	qp = container_of(ib_qp, struct usiw_qp, ib_qp);
-	switch (rte_atomic16_read(&qp->conn_state)) {
+	switch (rte_atomic16_read(&qp->shm_qp->conn_state)) {
 	case usiw_qp_connected:
+	case usiw_qp_running:
 		break;
 	case usiw_qp_shutdown:
 	case usiw_qp_error:
 		ret = EINVAL;
 		goto errout;
-	default:
-		/* This is an ugly hack to get around the fact that
-		 * ibv_modify_qp doesn't get called from userspace to let us
-		 * know about state changes.  We call ibv_modify_qp() ourselves
-		 * with attr_mask==0 to get the udata response that contains
-		 * the addressing information that we need about the
-		 * connection. */
-		sem_wait(&qp->conn_event_sem);
-		if (!qp_connected(qp)) {
-			ret = EINVAL;
-			goto errout;
-		}
-		break;
 	}
 	for (; wr != NULL; wr = wr->next) {
 		sge_limit = (wr->opcode == IBV_WR_RDMA_READ)
@@ -1166,7 +1195,7 @@ usiw_post_recv(struct ibv_qp *ib_qp, struct ibv_recv_wr *wr,
 	int x, ret;
 
 	qp = container_of(ib_qp, struct usiw_qp, ib_qp);
-	if (rte_atomic16_read(&qp->conn_state) == usiw_qp_error) {
+	if (rte_atomic16_read(&qp->shm_qp->conn_state) == usiw_qp_error) {
 		*bad_wr = wr;
 		return EINVAL;
 	}
@@ -1365,6 +1394,7 @@ int
 usiw_init_context(struct verbs_device *device, struct ibv_context *context,
 		int cmd_fd)
 {
+	static pthread_once_t progress_thread_once = PTHREAD_ONCE_INIT;
 	struct ibv_get_context cmd;
 	struct {
 		struct ibv_get_context_resp ibv;
@@ -1373,6 +1403,13 @@ usiw_init_context(struct verbs_device *device, struct ibv_context *context,
 	struct usiw_context *ctx;
 	struct usiw_device *dev;
 	int ret;
+
+	/* ibv_open_device requires a valid ibv_device, which can only be
+	 * obtained by calling ibv_get_device_list, which initializes every
+	 * device on the system.  Thus we are guaranteed that we have every
+	 * possible device initialized at this point and can start the progress
+	 * thread. */
+	pthread_once(&progress_thread_once, start_progress_thread);
 
 	context->cmd_fd = cmd_fd;
 	if ((ret = ibv_cmd_get_context(context, &cmd, sizeof(cmd),
@@ -1398,8 +1435,8 @@ usiw_init_context(struct verbs_device *device, struct ibv_context *context,
 	verbs_set_ctx_op(&ctx->vcontext, close_xrcd, &usiw_close_xrcd);
 
 	dev = container_of(device, struct usiw_device, vdev);
-	ctx->port = dev->port;
-	ctx->port->ctx = ctx;
+	ctx->dev = dev;
+	ctx->dev->ctx = ctx;
 
 	rte_atomic32_init(&ctx->qp_init_count);
 	LIST_INIT(&ctx->qp_active);

src/kmod/verbs.c
@@ -44,8 +44,6 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/vmalloc.h>
-#include <net/neighbour.h>
-#include <net/route.h>
 
 #include <rdma/iw_cm.h>
 #include <rdma/ib_verbs.h>
@@ -103,156 +101,13 @@ siw_mmap(struct ib_ucontext *ctx, struct vm_area_struct *vma)
 	return -ENOSYS;
 }
 
-static int fetch_dest_hwaddr(struct net_device *netdev,
-		u32 src_ipv4_addr, u32 dst_ipv4_addr, void *dst_hw_addr)
-{
-	struct neighbour *n;
-	struct rtable *rt;
-	struct flowi4 fl4;
-	int rv = 0;
-
-	memset(&fl4, 0, sizeof(fl4));
-	fl4.saddr = src_ipv4_addr;
-	fl4.daddr = dst_ipv4_addr;
-	fl4.flowi4_oif = netdev->ifindex;
-	rt = ip_route_output_key(&init_net, &fl4);
-	if (IS_ERR(rt)) {
-		return PTR_ERR(rt);
-	}
-
-	rcu_read_lock();
-	n = dst_neigh_lookup(&rt->dst, &dst_ipv4_addr);
-	if (!n || !(n->nud_state & NUD_VALID)) {
-		rv = -ENODATA;
-		goto out;
-	}
-	memcpy(dst_hw_addr, n->ha, ETHER_ADDR_LEN);
-	rcu_read_unlock();
-
-	if (n)
-		neigh_release(n);
-
-out:
-	ip_rt_put(rt);
-	return rv;
-}
-
-static ssize_t siw_event_file_read(struct file *filp, char __user *buf,
-		size_t count, loff_t *pos)
-{
-	struct siw_event_file *file;
-	struct urdma_qp_connected_event event;
-	struct net_device *netdev;
-	struct siw_cep *cep;
-	ssize_t rv;
-
-	if (count < sizeof(event)) {
-		return -EINVAL;
-	}
-
-	file = filp->private_data;
-	spin_lock_irq(&file->lock);
-	if (!file->ctx) {
-		rv = -EINVAL;
-		goto out;
-	}
-
-	while (list_empty(&file->established_list)) {
-		if (filp->f_flags & O_NONBLOCK) {
-			rv = -EAGAIN;
-			goto out;
-		}
-		spin_unlock_irq(&file->lock);
-
-		rv = wait_event_interruptible(file->wait_head,
-				(!list_empty(&file->established_list)));
-		if (rv < 0) {
-			return rv;
-		}
-
-		spin_lock_irq(&file->lock);
-		if (!file->ctx) {
-			rv = -EINVAL;
-			goto out;
-		}
-	}
-
-	cep = list_first_entry(&file->established_list,
-			struct siw_cep, established_entry);
-
-	memset(&event, 0, sizeof(event));
-	event.event_type = SIW_EVENT_QP_CONNECTED;
-	event.qp_id = cep->qp->ofa_qp.qp_num;
-	event.src_ipv4 = cep->llp.laddr.sin_addr.s_addr;
-	event.src_port = cep->llp.laddr.sin_port;
-	event.dst_ipv4 = cep->llp.raddr.sin_addr.s_addr;
-	event.dst_port = cep->llp.raddr.sin_port;
-	event.ord_max = cep->qp->attrs.orq_size;
-	event.ird_max = cep->qp->attrs.irq_size;
-
-	netdev = cep->sdev->netdev;
-	dev_hold(netdev);
-	spin_unlock_irq(&file->lock);
-
-	/* Must not have IRQs disabled when querying routing tables. */
-	rv = fetch_dest_hwaddr(netdev, event.src_ipv4, event.dst_ipv4,
-		&event.dst_ether);
-	if (copy_to_user(buf, &event, sizeof(event))) {
-		return -EFAULT;
-	}
-
-	spin_lock_irq(&file->lock);
-	dev_put(netdev);
-	list_del(&cep->established_entry);
-	list_add_tail(&cep->rtr_wait_entry, &file->rtr_wait_list);
-	spin_unlock_irq(&file->lock);
-
-	return sizeof(event);
-
-out:
-	spin_unlock_irq(&file->lock);
-	return rv;
-}
-
-static unsigned int siw_event_file_poll(struct file *filp,
-		struct poll_table_struct *poll_table)
-{
-	struct siw_event_file *file;
-	unsigned int rv = 0;
-
-	file = filp->private_data;
-	spin_lock_irq(&file->lock);
-	if (!file->ctx) {
-		rv = -EINVAL;
-		goto out;
-	}
-	spin_unlock_irq(&file->lock);
-
-	poll_wait(filp, &file->wait_head, poll_table);
-
-	spin_lock_irq(&file->lock);
-	if (!file->ctx) {
-		rv = -EINVAL;
-		goto out;
-	}
-	if (!list_empty(&file->established_list)) {
-		rv = POLLIN | POLLRDNORM;
-	}
-
-out:
-	spin_unlock_irq(&file->lock);
-	return rv;
-}
-
 static ssize_t siw_event_file_write(struct file *filp, const char __user *buf,
 		size_t count, loff_t *pos)
 {
 	struct siw_event_file *file;
 	struct urdma_event_storage event;
 	struct urdma_cq_event *cq_event;
-	struct urdma_qp_rtr_event *qp_rtr_event;
 	struct siw_cq *cq;
-	struct siw_qp *qp;
 	ssize_t rv;
 
 	if (count > sizeof(event)) {
@@ -281,18 +136,6 @@ static ssize_t siw_event_file_write(struct file *filp, const char __user *buf,
 		cq->ofa_cq.comp_handler(&cq->ofa_cq, cq->ofa_cq.cq_context);
 		siw_cq_put(cq);
 		break;
-	case SIW_EVENT_QP_RTR:
-		if (count != sizeof(*qp_rtr_event)) {
-			rv = -EINVAL;
-			goto out;
-		}
-		qp_rtr_event = (struct urdma_qp_rtr_event *)&event;
-		qp = siw_qp_id2obj(file->ctx->sdev, qp_rtr_event->qp_id);
-		list_del(&qp->cep->rtr_wait_entry);
-		spin_unlock_irq(&file->lock);
-		rv = siw_qp_rtr(qp->cep);
-		siw_qp_put(qp);
-		return (rv < 0) ? rv : count;
 	default:
 		pr_debug(" got invalid event type %u\n", event.event_type);
 		rv = -EINVAL;
@@ -309,16 +152,7 @@ out:
 static int siw_event_file_release(struct inode *ignored, struct file *filp)
 {
 	struct siw_event_file *file = filp->private_data;
-	struct siw_cep *cep;
-	struct list_head *pos, *next;
 
-	spin_lock_irq(&file->lock);
-	list_for_each_safe(pos, next, &file->rtr_wait_list) {
-		cep = list_entry(pos, struct siw_cep, rtr_wait_entry);
-		siw_qp_rtr_fail(cep);
-		list_del(pos);
-	}
-	spin_unlock_irq(&file->lock);
 	if (file->ctx) {
 		file->ctx->event_file = NULL;
 	}
@@ -328,9 +162,7 @@ static int siw_event_file_release(struct inode *ignored, struct file *filp)
 
 static struct file_operations siw_event_file_ops = {
 	.owner = THIS_MODULE,
-	.read = &siw_event_file_read,
 	.write = &siw_event_file_write,
-	.poll = &siw_event_file_poll,
 	.release = &siw_event_file_release,
 };
 
@@ -355,11 +187,8 @@ static struct file *siw_event_file_new(struct siw_ucontext *ctx, int *event_fd)
 	}
 	ctx->event_file->ctx = ctx;
 	spin_lock_init(&ctx->event_file->lock);
-	INIT_LIST_HEAD(&ctx->event_file->established_list);
-	INIT_LIST_HEAD(&ctx->event_file->rtr_wait_list);
-	init_waitqueue_head(&ctx->event_file->wait_head);
 	filp = anon_inode_getfile("[siwevent]", &siw_event_file_ops,
-			ctx->event_file, O_RDWR|O_NONBLOCK);
+			ctx->event_file, O_WRONLY|O_NONBLOCK);
 	if (IS_ERR(filp)) {
 		rv = PTR_ERR(filp);
 		goto free_event_file;
@@ -735,9 +564,13 @@ struct ib_qp *siw_create_qp(struct ib_pd *ofa_pd,
 			goto err_out_idr;
 		qp->attrs.irq_size = ureq.ird_max;
 		qp->attrs.orq_size = ureq.ord_max;
+		qp->attrs.urdma_devid = ureq.urdmad_dev_id;
+		qp->attrs.urdma_qp_id = ureq.urdmad_qp_id;
+		qp->attrs.urdma_rxq = ureq.rxq;
+		qp->attrs.urdma_txq = ureq.txq;
 
 		memset(&uresp, 0, sizeof uresp);
-		uresp.qp_id = QP_ID(qp);
+		uresp.kmod_qp_id = QP_ID(qp);
 
 		rv = ib_copy_to_udata(udata, &uresp, sizeof uresp);
 		if (rv)

configure.ac
@@ -160,6 +160,11 @@ PKG_CHECK_MODULES([JSON_C], [json-c])
 AC_CHECK_HEADERS([uthash.h], [], AC_MSG_ERROR([urdma requires uthash]))
 AC_HEADER_ASSERT
 
+AC_CHECK_HEADERS([sys/socket.h], [],
+		 [AC_MSG_ERROR([urdma requires sys/socket.h])])
+AC_CHECK_DECLS([SOCK_CLOEXEC], [], [], [AC_INCLUDES_DEFAULT()]
+[[#include <sys/socket.h>]])
+
 old_CFLAGS="${CFLAGS}"
 old_CPPFLAGS="${CPPFLAGS}"
 old_LDFLAGS="${LDFLAGS}"

src/kmod/cm.c
@@ -124,6 +124,7 @@ static struct siw_cep *siw_cep_alloc(struct siw_dev  *sdev)
 	if (cep) {
 		unsigned long flags;
 
+		INIT_LIST_HEAD(&cep->disconnect_entry);
 		INIT_LIST_HEAD(&cep->rtr_wait_entry);
 		INIT_LIST_HEAD(&cep->established_entry);
 		INIT_LIST_HEAD(&cep->listenq);

src/kmod/cm.h
@@ -87,6 +87,10 @@ struct siw_cep {
 	 */
 	struct list_head	listenq;
 
+	/* The list of connections that have been disconnected but we have not
+	 * yet notified userspace. */
+	struct list_head	disconnect_entry;
+
 	/* The list of connections that have been established but we have not
 	 * yet notified userspace. */
 	struct list_head	established_entry;
@@ -107,6 +111,8 @@ struct siw_cep {
 	struct list_head	work_freelist;
 	struct siw_llp_info	llp;
 	struct siw_mpa_info	mpa;
+	uint16_t		urdmad_dev_id;
+	uint16_t		urdmad_qp_id;
 	int			ord;
 	int			ird;
 	int			sk_error; /* not (yet) used XXX */

src/kmod/main.c
@@ -42,6 +42,8 @@
 #include <linux/netdevice.h>
 #include <linux/inetdevice.h>
 #include <net/net_namespace.h>
+#include <net/neighbour.h>
+#include <net/route.h>
 #include <linux/rtnetlink.h>
 #include <linux/if_arp.h>
 #include <linux/list.h>
@@ -94,6 +96,292 @@ static struct device usiw_generic_dma_device = {
 
 static struct bus_type usiw_bus = {
 	.name	= "urdma",
+};
+
+static int urdma_chardev_open(struct inode *inodep, struct file *filp)
+{
+	if (!try_module_get(THIS_MODULE)) {
+		return -ENXIO;
+	}
+
+	filp->private_data = dev_get_drvdata(&usiw_generic_dma_device);
+	return 0;
+}
+
+static int fetch_dest_hwaddr(struct net_device *netdev,
+		u32 src_ipv4_addr, u32 dst_ipv4_addr, void *dst_hw_addr)
+{
+	struct neighbour *n;
+	struct rtable *rt;
+	struct flowi4 fl4;
+	int rv = 0;
+
+	memset(&fl4, 0, sizeof(fl4));
+	fl4.saddr = src_ipv4_addr;
+	fl4.daddr = dst_ipv4_addr;
+	fl4.flowi4_oif = netdev->ifindex;
+	rt = ip_route_output_key(&init_net, &fl4);
+	if (IS_ERR(rt)) {
+		return PTR_ERR(rt);
+	}
+
+	rcu_read_lock();
+	n = dst_neigh_lookup(&rt->dst, &dst_ipv4_addr);
+	if (!n || !(n->nud_state & NUD_VALID)) {
+		rv = -ENODATA;
+		goto out;
+	}
+	memcpy(dst_hw_addr, n->ha, ETHER_ADDR_LEN);
+	rcu_read_unlock();
+
+	if (n)
+		neigh_release(n);
+
+out:
+	ip_rt_put(rt);
+	return rv;
+}
+
+static unsigned int urdma_chardev_poll(struct file *filp,
+		struct poll_table_struct *poll_table)
+{
+	struct urdma_chardev_data *file;
+	unsigned int rv = 0;
+
+	file = filp->private_data;
+	spin_lock_irq(&file->lock);
+	if (!file->dev) {
+		rv = -EINVAL;
+		goto out;
+	}
+	spin_unlock_irq(&file->lock);
+
+	poll_wait(filp, &file->wait_head, poll_table);
+
+	spin_lock_irq(&file->lock);
+	if (!file->dev) {
+		rv = -EINVAL;
+		goto out;
+	}
+	if (!list_empty(&file->established_list)
+			|| !list_empty(&file->disconnect_list)) {
+		rv = POLLIN | POLLRDNORM;
+	}
+
+out:
+	spin_unlock_irq(&file->lock);
+	return rv;
+}
+
+/* file->lock MUST be locked */
+static ssize_t do_read_disconnect_event(struct urdma_chardev_data *file,
+		struct siw_cep *cep, char *buf, size_t count)
+{
+	struct urdma_qp_disconnected_event event;
+
+	if (count < sizeof(event)) {
+		return -EINVAL;
+	}
+
+	memset(&event, 0, sizeof(event));
+	event.event_type = SIW_EVENT_QP_DISCONNECTED;
+	event.urdmad_dev_id = cep->urdmad_dev_id;
+	event.urdmad_qp_id = cep->urdmad_qp_id;
+	if (copy_to_user(buf, &event, sizeof(event))) {
+		return -EFAULT;
+	}
+
+	list_del(&cep->disconnect_entry);
+	siw_cep_put(cep);
+
+	return sizeof(event);
+} /* do_read_disconnect_event */
+
+/* file->lock MUST be locked */
+static ssize_t do_read_established_event(struct urdma_chardev_data *file,
+		struct siw_cep *cep, char *buf, size_t count)
+{
+	struct urdma_qp_connected_event event;
+	struct net_device *netdev;
+	ssize_t rv;
+
+	if (count < sizeof(event)) {
+		return -EINVAL;
+	}
+
+	memset(&event, 0, sizeof(event));
+	event.event_type = SIW_EVENT_QP_CONNECTED;
+	event.kmod_qp_id = cep->qp->ofa_qp.qp_num;
+	event.urdmad_dev_id = cep->qp->attrs.urdma_devid;
+	event.urdmad_qp_id = cep->qp->attrs.urdma_qp_id;
+	event.src_ipv4 = cep->llp.laddr.sin_addr.s_addr;
+	event.src_port = cep->llp.laddr.sin_port;
+	event.dst_ipv4 = cep->llp.raddr.sin_addr.s_addr;
+	event.dst_port = cep->llp.raddr.sin_port;
+	event.ord_max = cep->qp->attrs.orq_size;
+	event.ird_max = cep->qp->attrs.irq_size;
+	event.rxq = cep->qp->attrs.urdma_rxq;
+	event.txq = cep->qp->attrs.urdma_txq;
+
+	netdev = cep->sdev->netdev;
+	dev_hold(netdev);
+	spin_unlock_irq(&file->lock);
+
+	/* Must not have IRQs disabled when querying routing tables. */
+	/* FIXME: do something with rv */
+	rv = fetch_dest_hwaddr(netdev, event.src_ipv4, event.dst_ipv4,
+		&event.dst_ether);
+	if (copy_to_user(buf, &event, sizeof(event))) {
+		spin_lock_irq(&file->lock);
+		return -EFAULT;
+	}
+
+	spin_lock_irq(&file->lock);
+	dev_put(netdev);
+	list_del(&cep->established_entry);
+	list_add_tail(&cep->rtr_wait_entry, &file->rtr_wait_list);
+
+	return sizeof(event);
+} /* do_read_established_event */
+
+static ssize_t urdma_chardev_read(struct file *filp, char *buf,
+		size_t count, loff_t *offset)
+{
+	struct urdma_chardev_data *file;
+	struct siw_cep *cep;
+	ssize_t rv;
+
+	file = filp->private_data;
+	spin_lock_irq(&file->lock);
+	if (!file->dev) {
+		rv = -EINVAL;
+		goto out;
+	}
+
+	while (list_empty(&file->established_list)
+			&& list_empty(&file->disconnect_list)) {
+		if (filp->f_flags & O_NONBLOCK) {
+			rv = -EAGAIN;
+			goto out;
+		}
+		spin_unlock_irq(&file->lock);
+
+		rv = wait_event_interruptible(file->wait_head,
+				(!list_empty(&file->established_list)
+				 ||!list_empty(&file->disconnect_list)));
+		if (rv < 0) {
+			return rv;
+		}
+
+		spin_lock_irq(&file->lock);
+		if (!file->dev) {
+			rv = -EINVAL;
+			goto out;
+		}
+	}
+
+	if (!list_empty(&file->established_list)) {
+		cep = list_first_entry(&file->established_list,
+				struct siw_cep, established_entry);
+		rv = do_read_established_event(file, cep, buf, count);
+	} else {
+		cep = list_first_entry(&file->disconnect_list,
+				struct siw_cep, disconnect_entry);
+		rv = do_read_disconnect_event(file, cep, buf, count);
+	}
+
+out:
+	spin_unlock_irq(&file->lock);
+	return rv;
+}
+
+static ssize_t urdma_chardev_write(struct file *filp, const char *buf,
+		size_t count, loff_t *offset)
+{
+	struct urdma_chardev_data *file;
+	struct urdma_event_storage event;
+	struct urdma_qp_rtr_event *qp_rtr_event;
+	struct list_head *lptr;
+	struct siw_cep *cep;
+	struct siw_qp *qp;
+	ssize_t rv;
+
+	if (count > sizeof(event)) {
+		return -EINVAL;
+	}
+
+	if (copy_from_user(&event, buf, count)) {
+		return -EFAULT;
+	}
+
+	file = filp->private_data;
+	spin_lock_irq(&file->lock);
+	if (!file->dev) {
+		rv = -EINVAL;
+		goto out;
+	}
+
+	switch (event.event_type) {
+	case SIW_EVENT_QP_RTR:
+		if (count != sizeof(*qp_rtr_event)) {
+			rv = -EINVAL;
+			goto out;
+		}
+		qp_rtr_event = (struct urdma_qp_rtr_event *)&event;
+		qp = NULL;
+		list_for_each(lptr, &file->rtr_wait_list) {
+			cep = list_entry(lptr, struct siw_cep, rtr_wait_entry);
+			if (cep->qp->hdr.id == qp_rtr_event->kmod_qp_id) {
+				qp = cep->qp;
+				siw_qp_get(qp);
+				list_del(&qp->cep->rtr_wait_entry);
+				break;
+			}
+		}
+		spin_unlock_irq(&file->lock);
+		if (qp) {
+			rv = siw_qp_rtr(qp->cep);
+			siw_qp_put(qp);
+			return (rv < 0) ? rv : count;
+		} else {
+			return -EINVAL;
+		}
+	default:
+		pr_debug(" got invalid event type %u\n", event.event_type);
+		rv = -EINVAL;
+		goto out;
+	}
+
+	rv = count;
+
+out:
+	spin_unlock_irq(&file->lock);
+	return rv;
+}
+
+static int urdma_chardev_release(struct inode *inodep, struct file *filp)
+{
+	struct urdma_chardev_data *file = filp->private_data;
+	struct siw_cep *cep;
+	struct list_head *pos, *next;
+
+	spin_lock_irq(&file->lock);
+	list_for_each_safe(pos, next, &file->rtr_wait_list) {
+		cep = list_entry(pos, struct siw_cep, rtr_wait_entry);
+		list_del(pos);
+	}
+	spin_unlock_irq(&file->lock);
+	module_put(THIS_MODULE);
+	return 0;
+}
+
+static struct file_operations urdma_fops =
+{
+	.open = urdma_chardev_open,
+	.poll = urdma_chardev_poll,
+	.read = urdma_chardev_read,
+	.write = urdma_chardev_write,
+	.release = urdma_chardev_release,
 };
 
 static int siw_modify_port(struct ib_device *ofa_dev, u8 port, int mask,
@@ -185,6 +473,7 @@ static void siw_device_destroy(struct siw_dev *sdev)
 			sdev->ofa_dev.name,
 			sdev->netdev ? sdev->netdev->name : "(unattached)");
 
+	put_device(sdev->ofa_dev.dma_device);
 	siw_idr_release(sdev);
 	kfree(sdev->ofa_dev.iwcm);
 	if (sdev->netdev)
@@ -293,7 +582,7 @@ static struct siw_dev *siw_device_create(int list_index)
 	ofa_dev->phys_port_cnt = 1;
 
 	ofa_dev->num_comp_vectors = 1;
-	ofa_dev->dma_device = &usiw_generic_dma_device;
+	ofa_dev->dma_device = get_device(&usiw_generic_dma_device);
 	ofa_dev->query_device = siw_query_device;
 	ofa_dev->query_port = siw_query_port;
 #ifdef HAVE_IB_GET_PORT_IMMUTABLE
@@ -493,7 +782,30 @@ static struct notifier_block siw_netdev_nb = {
  */
 static __init int siw_init_module(void)
 {
+	struct urdma_chardev_data *chardev_data;
 	int x, rv;
+	dev_t devt;
+
+	chardev_data = kmalloc(sizeof(*chardev_data), GFP_KERNEL);
+	if (!chardev_data) {
+		rv = -ENOMEM;
+		goto out_nochardev;
+	}
+	spin_lock_init(&chardev_data->lock);
+	INIT_LIST_HEAD(&chardev_data->established_list);
+	INIT_LIST_HEAD(&chardev_data->disconnect_list);
+	INIT_LIST_HEAD(&chardev_data->rtr_wait_list);
+	init_waitqueue_head(&chardev_data->wait_head);
+	dev_set_drvdata(&usiw_generic_dma_device, chardev_data);
+
+	rv = register_chrdev(0, "urdma", &urdma_fops);
+	if (rv < 0) {
+		/* This gets implicitly cleaned up by unregister_chrdev once it
+		 * is registered. */
+		kfree(chardev_data);
+		goto out_nochardev;
+	}
+	usiw_generic_dma_device.devt = devt = MKDEV(rv, 0);
 
 	/*
 	 * The xprtrdma module needs at least some rudimentary bus to set
@@ -507,7 +819,7 @@ static __init int siw_init_module(void)
 
 	rv = device_register(&usiw_generic_dma_device);
 	if (rv)
-		goto out;
+		goto out_nodev;
 
 	rv = siw_cm_init();
 	if (rv)
@@ -552,19 +864,22 @@ out_free_devlist:
 out_unregister:
 	device_unregister(&usiw_generic_dma_device);
 
-out:
+out_nodev:
 	bus_unregister(&usiw_bus);
 out_nobus:
+	unregister_chrdev(MAJOR(devt), "urdma");
 	pr_info("urdma kernel support attach failed. Error: %d\n",
 			rv);
 	siw_cm_exit();
 
+out_nochardev:
 	return rv;
 }
 
 
 static void __exit siw_exit_module(void)
 {
+	struct urdma_chardev_data *file;
 	int x;
 
 	spin_lock(&siw_dev_lock);
@@ -587,6 +902,10 @@ static void __exit siw_exit_module(void)
 	device_unregister(&usiw_generic_dma_device);
 
 	bus_unregister(&usiw_bus);
+	unregister_chrdev(MAJOR(usiw_generic_dma_device.devt), "urdma");
+
+	file = dev_get_drvdata(&usiw_generic_dma_device);
+	kfree(file);
 
 	pr_info("urdma kernel support detached\n");
 }

commit 319e0fcd816da50691ae2d6c77216e0b4582bfbf

Use calloc() instead of malloc() to allocate queue pairs
Ensures that the whole structure is zeroed out, which may not always be
the case.  This fixes a bug where the recv_ack_psn was not properly
initialized and the first incoming packet was thus assumed to be a
duplicate.

Fixes: 18667e1c0ad9 ("liburdma, kmod: Update for urdmad")
Signed-off-by: Patrick MacArthur <patrick@patrickmacarthur.net>

src/liburdma/verbs.c
@@ -6,6 +6,7 @@
  * Authors: Patrick MacArthur <pam@zurich.ibm.com>
  *
  * Copyright (c) 2016, IBM Corporation
+ * Copyright (c) 2016-2017, University of New Hampshire InterOperability Laboratory
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -822,7 +823,7 @@ usiw_create_qp(struct ibv_pd *pd, struct ibv_qp_init_attr *qp_init_attr)
 
 	ctx = usiw_get_context(pd->context);
 
-	qp = malloc(sizeof(*qp));
+	qp = calloc(1, sizeof(*qp));
 	if (!qp) {
 		goto errout;
 	}