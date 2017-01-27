zrlio/urdma

commit 9de37f6029bcdb5837d4e55bbfedec3040aed035

liburdma: Remove pointer from device to context
Instead of looping over ibv_devices in the main progress thread loop,
loop over ibv_contexts, thus removing the pointer from the device to the
context.  This makes more sense because there can be multiple contexts
open for a single device.

This also fixes a long-standing bug where we leaked a dangling pointer
to a context after the context was closed with ibv_close_device(),
leading to a segmentation fault in the progress thread.  This issue was
hidden for a long time since the RDMA CM creates long-lived contexts
that applications should use and never closes them, but openmpi creates
and closes multiple contexts during its execution.

Signed-off-by: Patrick MacArthur <patrick@patrickmacarthur.net>

src/liburdma/driver.c
@@ -80,6 +80,12 @@
 static struct usiw_driver *driver;
 
 void
+driver_add_context(struct usiw_context *ctx)
+{
+	LIST_INSERT_HEAD(&driver->ctxs, ctx, driver_entry);
+} /* driver_add_context */
+
+void
 start_progress_thread(void)
 {
 	sem_post(&driver->go);
@@ -232,7 +238,6 @@ usiw_driver_init(int portid)
 
 	dev->urdmad_fd = driver->urdmad_fd;
 
-	LIST_INSERT_HEAD(&driver->devs, dev, driver_entry);
 	return &dev->vdev.device;
 } /* usiw_driver_init */
 
@@ -471,7 +476,7 @@ do_init_driver(void)
 	driver = calloc(1, sizeof(*driver));
 	if (!driver)
 		goto err;
-	LIST_INIT(&driver->devs);
+	LIST_INIT(&driver->ctxs);
 
 	driver->urdmad_fd = setup_socket(sock_name);
 	if (driver->urdmad_fd < 0)

src/liburdma/interface.c
@@ -2117,7 +2117,7 @@ int
 kni_loop(void *arg)
 {
 	struct usiw_driver *driver = arg;
-	struct usiw_device *dev, **dev_prev;
+	struct usiw_context *ctx, **ctx_prev;
 	struct usiw_qp *qp, **qp_prev;
 	struct rte_mbuf *rxmbuf[RX_BURST_SIZE];
 	unsigned int count;
@@ -2125,13 +2125,8 @@ kni_loop(void *arg)
 
 	sem_wait(&driver->go);
 	while (1) {
-		LIST_FOR_EACH(dev, &driver->devs, driver_entry, dev_prev) {
-			if (!dev->ctx) {
-				continue;
-			}
-
-			LIST_FOR_EACH(qp, &dev->ctx->qp_active,
-							ctx_entry, qp_prev) {
+		LIST_FOR_EACH(ctx, &driver->ctxs, driver_entry, ctx_prev) {
+			LIST_FOR_EACH(qp, &ctx->qp_active, ctx_entry, qp_prev) {
 				switch (rte_atomic16_read(&qp->shm_qp->conn_state)) {
 				case usiw_qp_connected:
 					/* start_qp() transitions to

src/liburdma/interface.h
@@ -313,6 +313,7 @@ struct usiw_context {
 	struct verbs_context vcontext;
 	struct usiw_device *dev;
 	int event_fd;
+	LIST_ENTRY(usiw_context) driver_entry;
 	LIST_HEAD(usiw_qp_head, usiw_qp) qp_active;
 	rte_atomic32_t qp_init_count;
 		/**< The number of queue pairs in the INIT state. */
@@ -332,7 +333,6 @@ usiw_get_context(struct ibv_context *ctx)
 
 struct usiw_device {
 	struct verbs_device vdev;
-	LIST_ENTRY(usiw_device) driver_entry;
 	struct rte_mempool *rx_mempool;
 	struct rte_mempool *tx_ddp_mempool;
 	struct rte_mempool *tx_hdr_mempool;
@@ -352,7 +352,7 @@ struct usiw_driver {
 	struct nl_sock *sock;
 	struct nl_cache *link_cache;
 	struct nl_cache *addr_cache;
-	LIST_HEAD(usiw_device_list_head, usiw_device) devs;
+	LIST_HEAD(usiw_context_list_head, usiw_context) ctxs;
 	int urdmad_fd;
 	uint32_t lcore_mask[RTE_MAX_LCORE / 32];
 };
@@ -360,6 +360,11 @@ struct usiw_driver {
 /** Starts the progress thread. */
 void
 start_progress_thread(void);
+
+/** Adds an IB user context to the list of contexts to be managed by the
+ * progress thread. */
+void
+driver_add_context(struct usiw_context *ctx);
 
 struct usiw_mr **
 usiw_mr_lookup(struct usiw_mr_table *tbl, uint32_t rkey);

src/liburdma/verbs.c
@@ -1437,7 +1437,6 @@ usiw_init_context(struct verbs_device *device, struct ibv_context *context,
 
 	dev = container_of(device, struct usiw_device, vdev);
 	ctx->dev = dev;
-	ctx->dev->ctx = ctx;
 
 	rte_atomic32_init(&ctx->qp_init_count);
 	LIST_INIT(&ctx->qp_active);
@@ -1445,12 +1444,16 @@ usiw_init_context(struct verbs_device *device, struct ibv_context *context,
 	ctx->qp = NULL;
 	rte_spinlock_init(&ctx->qp_lock);
 
+	driver_add_context(ctx);
 	return 0;
 } /* usiw_init_context */
 
 
 void
-usiw_uninit_context(__attribute__((unused)) struct verbs_device *device,
-		__attribute__((unused)) struct ibv_context *ib_ctx)
+usiw_uninit_context(struct verbs_device *device, struct ibv_context *ib_ctx)
 {
+	struct usiw_context *ctx = usiw_get_context(ib_ctx);
+	RTE_LOG(INFO, USER1, "close context %p for device %p\n",
+			(void *)ib_ctx, (void *)device);
+	LIST_REMOVE(ctx, driver_entry);
 } /* usiw_uninit_context */

commit 64ecca2e97cc3bebf855c58ac54081b77d01ad1e

liburdma: Manipulate driver context list in progress thread only
Commit 9de37f6029bc ("liburdma: Remove pointer from device to context")
introduced a race condition when opening a new context with
ibv_open_device(), where the user thread would add an entry to the
driver context list but the progress thread would never see the added
context.  This is because the sys/queue.h interface is not thread-safe.
This worked prior to this commit because the set of drivers was static
and finalized before the progress thread starts, whereas contexts are
opened/destroyed by the user application outside of our control.

To fix this, we must use alternate mechanisms to inform the progress
thread of new and removed contexts.  We use a DPDK ring to inform the
progress thread that a context has been added, and new contexts are
added prior to the next iteration of the main loop.

Handling closed (destroyed) contexts is challenging, because
ibv_close_device() frees the context structure, but we can't directly
remove the context from the progress thread's list since the list is not
thread safe.  To solve this problem, we make the progress thread's
context list indirect; the list consists of handles which contain an
atomic pointer to the context.  When the context is destroyed with
ibv_close_device(), we atomically set the pointer to NULL, and then the
progress thread removes the handle from the list and destroys it the next
time it goes around the loop.

Fixes: 9de37f6029bc ("liburdma: Remove pointer from device to context")
Signed-off-by: Patrick MacArthur <patrick@patrickmacarthur.net>

src/liburdma/driver.c
@@ -79,10 +79,15 @@
 
 static struct usiw_driver *driver;
 
-void
+int
 driver_add_context(struct usiw_context *ctx)
 {
-	LIST_INSERT_HEAD(&driver->ctxs, ctx, driver_entry);
+	int i, ret;
+	ret = rte_ring_enqueue(driver->new_ctxs, ctx->h);
+	for (i = 0; ret == -ENOBUFS && i < 1000; ++i) {
+		ret = rte_ring_enqueue(driver->new_ctxs, ctx->h);
+	}
+	return ret;
 } /* driver_add_context */
 
 void
@@ -473,7 +478,8 @@ do_init_driver(void)
 		return;
 	}
 
-	driver = calloc(1, sizeof(*driver));
+	driver = calloc(1, sizeof(*driver) + rte_ring_get_memsize(
+							NEW_CTX_MAX + 1));
 	if (!driver)
 		goto err;
 	LIST_INIT(&driver->ctxs);
@@ -485,7 +491,7 @@ do_init_driver(void)
 	if (do_hello() < 0) {
 		fprintf(stderr, "Could not setup socket: %s\n",
 				strerror(errno));
-		goto err;
+		goto free_ring;
 	}
 	eal_argv[eal_argc - 1] = format_coremask(driver->lcore_mask,
 						 RTE_DIM(driver->lcore_mask));
@@ -514,8 +520,17 @@ do_init_driver(void)
 	}
 	free(eal_argv);
 
-	if (sem_init(&driver->go, 0, 0))
+	driver->new_ctxs = (struct rte_ring *)(driver + 1);
+	ret = rte_ring_init(driver->new_ctxs, "new_ctx_ring", NEW_CTX_MAX + 1,
+			    RING_F_SC_DEQ);
+	if (ret < 0) {
+		RTE_LOG(ERR, USER1, "cannot allocate new context ring: %s\n",
+				rte_strerror(ret));
 		goto close_fd;
+	}
+
+	if (sem_init(&driver->go, 0, 0))
+		goto free_ring;
 	ret = rte_eal_remote_launch(kni_loop, driver,
 			rte_get_next_lcore(rte_get_master_lcore(), 1, 1));
 	if (ret) {
@@ -525,6 +540,8 @@ do_init_driver(void)
 
 destroy_sem:
 	sem_destroy(&driver->go);
+free_ring:
+	rte_ring_free(driver->new_ctxs);
 close_fd:
 	close(driver->urdmad_fd);
 err:

src/liburdma/interface.c
@@ -2100,16 +2100,32 @@ find_matching_qp(struct usiw_context *ctx, struct rte_mbuf *pkt)
 int
 kni_loop(void *arg)
 {
-	struct usiw_driver *driver = arg;
-	struct usiw_context *ctx, **ctx_prev;
+	struct usiw_context_handle *h, **h_prev;
+	struct usiw_context *ctx;
+	struct usiw_driver *driver;
 	struct usiw_qp *qp, **qp_prev;
 	struct rte_mbuf *rxmbuf[RX_BURST_SIZE];
-	unsigned int count;
+	void *ctxs_to_add[NEW_CTX_MAX];
+	unsigned int i, count;
 	int portid, ret;
 
+	driver = arg;
 	sem_wait(&driver->go);
 	while (1) {
-		LIST_FOR_EACH(ctx, &driver->ctxs, driver_entry, ctx_prev) {
+		count = rte_ring_dequeue_burst(driver->new_ctxs, ctxs_to_add,
+					     NEW_CTX_MAX);
+		for (i = 0; i < count; ++i) {
+			h = (struct usiw_context_handle *)ctxs_to_add[i];
+			LIST_INSERT_HEAD(&driver->ctxs, h, driver_entry);
+		}
+
+		LIST_FOR_EACH(h, &driver->ctxs, driver_entry, h_prev) {
+			ctx = (void *)atomic_load(&h->ctxp);
+			if (unlikely(!ctx)) {
+				LIST_REMOVE(h, driver_entry);
+				free(h);
+				continue;
+			}
 			LIST_FOR_EACH(qp, &ctx->qp_active, ctx_entry, qp_prev) {
 				switch (atomic_load(&qp->shm_qp->conn_state)) {
 				case usiw_qp_connected:

src/liburdma/interface.h
@@ -74,6 +74,9 @@
 #define USIW_IRD_MAX 128
 #define USIW_ORD_MAX 128
 
+/* MUST be a power of 2 minus 1 */
+#define NEW_CTX_MAX 31
+
 #define STAG_TYPE_MASK      UINT32_C(0xFF000000)
 #define STAG_MASK           UINT32_C(0x00FFFFFF)
 #define STAG_TYPE_MR        (UINT32_C(0x00) << 24)
@@ -310,11 +313,22 @@ enum usiw_device_flags {
 	port_fdir = 2,
 };
 
+/* A context handle which provides an indirection for accessing the actual
+ * context from the progress thread.  The reason we need this is that the
+ * context may be freed by the verbs layer while we still have a reference to
+ * it.  When the user calls ibv_close_device(3), we atomically set the pointer
+ * in this handle to 0.  The progress thread then removes the entry from the
+ * list and frees it. */
+struct usiw_context_handle {
+	LIST_ENTRY(usiw_context_handle) driver_entry;
+	atomic_uintptr_t ctxp;
+};
+
 struct usiw_context {
 	struct verbs_context vcontext;
 	struct usiw_device *dev;
 	int event_fd;
-	LIST_ENTRY(usiw_context) driver_entry;
+	struct usiw_context_handle *h;
 	LIST_HEAD(usiw_qp_head, usiw_qp) qp_active;
 	atomic_uint qp_init_count;
 		/**< The number of queue pairs in the INIT state. */
@@ -352,7 +366,8 @@ struct usiw_driver {
 	struct nl_sock *sock;
 	struct nl_cache *link_cache;
 	struct nl_cache *addr_cache;
-	LIST_HEAD(usiw_context_list_head, usiw_context) ctxs;
+	LIST_HEAD(usiw_context_handle_list_head, usiw_context_handle) ctxs;
+	struct rte_ring *new_ctxs;
 	int urdmad_fd;
 	uint32_t lcore_mask[RTE_MAX_LCORE / 32];
 };
@@ -363,7 +378,7 @@ start_progress_thread(void);
 
 /** Adds an IB user context to the list of contexts to be managed by the
  * progress thread. */
-void
+int
 driver_add_context(struct usiw_context *ctx);
 
 struct usiw_mr **

src/liburdma/verbs.c
@@ -1423,7 +1423,8 @@ usiw_init_context(struct verbs_device *device, struct ibv_context *context,
 		RTE_LOG(DEBUG, USER1, "ibv_cmd_get_context failed: %s\n",
 				strerror(ret));
 		errno = ret;
-		return -1;
+		ret = -1;
+		goto out;
 	}
 
 	ctx = usiw_get_context(context);
@@ -1449,8 +1450,32 @@ usiw_init_context(struct verbs_device *device, struct ibv_context *context,
 	ctx->qp = NULL;
 	rte_spinlock_init(&ctx->qp_lock);
 
-	driver_add_context(ctx);
+	/* Context handle is used to deal with freed contexts after they have
+	 * been destroyed. */
+	ctx->h = malloc(sizeof(*ctx->h));
+	if (!ctx->h) {
+		RTE_LOG(ERR, USER1, "handle for context %p allocation failed: %s\n",
+				(void *)&ctx->vcontext.context,
+				strerror(errno));
+		ret = -1;
+		goto out;
+	}
+	atomic_init(&ctx->h->ctxp, (uintptr_t)ctx);
+
+	ret = driver_add_context(ctx);
+	if (unlikely(ret < 0)) {
+		if (ret != -EDQUOT) {
+			errno = ret;
+			ret = -1;
+			goto free_ctx;
+		}
+	}
 	return 0;
+
+free_ctx:
+	free(ctx->h);
+out:
+	return ret;
 } /* usiw_init_context */
 
 
@@ -1458,5 +1483,5 @@ void
 usiw_uninit_context(struct verbs_device *device, struct ibv_context *ib_ctx)
 {
 	struct usiw_context *ctx = usiw_get_context(ib_ctx);
-	LIST_REMOVE(ctx, driver_entry);
+	atomic_store(&ctx->h->ctxp, 0);
 } /* usiw_uninit_context */